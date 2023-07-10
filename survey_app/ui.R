c299e0ff8732535217cd89b97e54ccf4cd3cca45
Jessica Guo <jessicaguo@email.arizona.edu>
2023-07-10 16:56:09 -0700

    Remove app to separate document

--- survey_app/ui.R
+++
@@ -1,8 +0,0 @@
-
-fluidPage(
-  theme = bs_theme(version = 3),
-  surveyOutput(df = df,
-               survey_title = "Pre-submission survey",
-               survey_description = h4(HTML("Thank you for your interest in contributing to PSInet. Please complete the following form <b>for a single dataset</b> and we will contact you with further submission details.")),
-               theme = "#348DBC")
-)
